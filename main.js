8b029ee67b6c07d783f158c60387cc2fb7e0a7ca
Genaro <genarorosti@hotmail.com>
2024-06-27 02:44:50 -0300

    varios

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const pokedex = document.querySelector("#pokedex");
 const botonesHeader = document.querySelectorAll(".btn-header");
 
-for (let i = 1; i <= 251; i++) {
+for (let i = 1; i <= 493; i++) {
     fetch("https://pokeapi.co/api/v2/pokemon/" + i)
         .then((response) => response.json())
         .then(data => mostrarPokemon(data))

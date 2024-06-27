e23d8e1fb24e6af9dacba6c282bd1a0d443dad56
Leandro Insua <leooinsua79@gmail.com>
2024-06-26 23:37:06 -0300

    commit back

--- equipo.js
+++ equipo.js
@@ -1,4 +1,6 @@
 const randomBtn = document.getElementById("randomBtn");
+const pokeCard = document.getElementById("pokeCard");
+let contenido = "";
 
 function numeroRandom(min, max) {
     const minimo = Math.ceil(min);
@@ -6,16 +8,31 @@ function numeroRandom(min, max) {
     return Math.floor(Math.random() * (maximo - minimo) + minimo);
   }
   
-  const llamarApi = () => {
-    num = numeroRandom(1, 1030);
-    fetch("https://pokeapi.co/api/v2/pokemon/" + num)
-        .then(res => res.json())
-        .then(data => console.log(data)
-            )
-        .catch(e => console.error(new Error(e)));
-  
+const llamarApi = () => {
+    for (let index = 1; index <= 6; index++) {
+      
+      num = numeroRandom(1, 1030);
+      fetch("https://pokeapi.co/api/v2/pokemon/" + num)
+          .then(res => res.json())
+          .then(data => crearHtml(data)
+              )
+          .catch(e => console.error(new Error(e)));
+    
+      
+    }
     
   }
+
+  function crearHtml(pokemon) {
+    contenido+=`
+     <h4 class="titulo">${pokemon.name}</h4>
+      <img class="pokemon" src="${pokemon.sprites.front_default}">
+    `;
+    pokeCard.innerHTML = contenido;
+
+  }
+
+  
   
   
   randomBtn.addEventListener("click", llamarApi)
